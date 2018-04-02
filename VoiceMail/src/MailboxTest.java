5a23cda53fe645b700406110f060633954702b2f
César Ivan Delgado Silva <ceides9497@gmail.com>
2018-04-02 02:38:58 -0400

    Creando Before

--- VoiceMail/src/MailboxTest.java
+++ VoiceMail/src/MailboxTest.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MailboxTest {
 
+	Mailbox mailbox;
+	String passcode;
+	String greeting;
+	
+	@Before
+	public void init(){
+		passcode = "1";
+		greeting = "Hola!";
+		mailbox = new Mailbox(passcode,greeting);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
