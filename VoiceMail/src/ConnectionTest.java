0401caabb794de4bc5ce66dc8d312f0f57ed07fa
SamuelUCB <val97samuel@hotmail.com>
2018-04-08 10:45:10 -0300

    ConnectionTest modified

--- VoiceMail/src/ConnectionTest.java
+++ VoiceMail/src/ConnectionTest.java
@@ -10,12 +10,12 @@ public class ConnectionTest {
 	
 	@Test
 	public void deberiaEjecutarseElMetodoSpeakDeTelephoneAlCrearUnConnection() {
-		//mailsystem = mock(MailSystem.class);
-		//telephone = mock(Telephone.class);
+		mailsystem = mock(MailSystem.class);
+		telephone = mock(Telephone.class);
 		
-		//doNothing().when(telephone).updateInterface(isA(String.class));
+		doNothing().when(telephone).updateInterface(isA(String.class));
 		
-		//telephone.updateInterface("mensaje inicial");
+		telephone.updateInterface("mensaje inicial");
 		
 		//connection = new Connection(mailsystem, telephone);
 		
