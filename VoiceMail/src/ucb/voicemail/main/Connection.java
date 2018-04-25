f963f67aed6be5fd045e8fc7b16f5778ad9e449b
SamuelUCB <val97samuel@hotmail.com>
2018-04-25 06:00:16 -0300

    Deleted state variable in Connection

--- VoiceMail/src/ucb/voicemail/main/Connection.java
+++ VoiceMail/src/ucb/voicemail/main/Connection.java
@@ -26,7 +26,6 @@ public class Connection implements Subject {
     private void resetConnection() {
         currentRecording = "";
         accumulatedKeys = "";
-        state = CONNECTED;
         connectionState = new ConnectedState();
         speakToAll(INITIAL_PROMPT);
     }
@@ -35,7 +34,6 @@ public class Connection implements Subject {
         if (key.equals("#")) {
             currentMailbox = system.findMailbox(accumulatedKeys);
             if (currentMailbox != null) {
-                state = RECORDING;
                 connectionState = new RecordingState();
                 speakToAll(currentMailbox.getGreeting());
             }
@@ -52,7 +50,6 @@ public class Connection implements Subject {
     public void login(String key) { // private -> public
         if (key.equals("#")) {
             if (currentMailbox.checkPasscode(accumulatedKeys)) {
-                state = MAILBOX_MENU;
                 connectionState = new MailboxMenuState();
                 speakToAll(MAILBOX_MENU_TEXT);
             }
@@ -69,7 +66,6 @@ public class Connection implements Subject {
     public void changePasscode(String key) { // private -> public
         if (key.equals("#")) {
             currentMailbox.setPasscode(accumulatedKeys);
-            state = MAILBOX_MENU;
             connectionState = new MailboxMenuState();
             speakToAll(MAILBOX_MENU_TEXT);
             accumulatedKeys = "";
@@ -83,7 +79,6 @@ public class Connection implements Subject {
         if (key.equals("#")) {
             currentMailbox.setGreeting(currentRecording);
             currentRecording = "";
-            state = MAILBOX_MENU;
             connectionState = new MailboxMenuState();
             speakToAll(MAILBOX_MENU_TEXT);
         }
@@ -91,17 +86,14 @@ public class Connection implements Subject {
 
     public void mailboxMenu(String key) { // private -> public
         if (key.equals("1")) {
-            state = MESSAGE_MENU;
             connectionState = new MessageMenuState();
             speakToAll(MESSAGE_MENU_TEXT);
         }
         else if (key.equals("2")) {
-            state = CHANGE_PASSCODE;
             connectionState = new ChangePasscodeState();
             speakToAll("Enter new passcode followed by the # key");
         }
         else if (key.equals("3")) {
-            state = CHANGE_GREETING;
             connectionState = new ChangeGreetingState();
             speakToAll("Record your greeting, then press the # key");
         }
@@ -129,7 +121,6 @@ public class Connection implements Subject {
             speakToAll(MESSAGE_MENU_TEXT);
         }
         else if (key.equals("4")) {
-            state = MAILBOX_MENU;
             connectionState = new MailboxMenuState();
             speakToAll(MAILBOX_MENU_TEXT);
         }
@@ -180,18 +171,8 @@ public class Connection implements Subject {
     private Mailbox currentMailbox;
     private String currentRecording;
     private String accumulatedKeys;
-   	private ConsoleTelephone phone;
-   	private int state;
     private ArrayList<Telephone> userInterfaces;
     private ConnectionState connectionState;
-    
-    private static final int DISCONNECTED = 0;
-   	private static final int CONNECTED = 1;
-   	private static final int RECORDING = 2;
-   	private static final int MAILBOX_MENU = 3;
-   	private static final int MESSAGE_MENU = 4;
-   	private static final int CHANGE_PASSCODE = 5;
-   	private static final int CHANGE_GREETING = 6;
 
    	private static final String INITIAL_PROMPT = 
         "Enter mailbox number followed by #";
