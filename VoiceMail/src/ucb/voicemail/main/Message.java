3c1149d5ce859349573f136ba487ce13885ff82d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-04-24 21:36:00 -0400

    Comentarios eliminados

--- VoiceMail/src/ucb/voicemail/main/Message.java
+++ VoiceMail/src/ucb/voicemail/main/Message.java
@@ -1,22 +1,11 @@
 package ucb.voicemail.main;
-/**
-   A message left by the caller.
-*/
 public class Message
 {
-   /**
-      Construct a Message object.
-      @param messageText the message text
-   */
    public Message(String messageText)
    {
       text = messageText;
    }
 
-   /**
-      Get the message text.
-      @return message text
-   */
    public String getText()
    {
       return text;
