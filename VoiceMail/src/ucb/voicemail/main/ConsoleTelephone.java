4a3051843c64fd67ceaffb8f7c2ae90ee9fe2c0c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-04-25 08:56:26 -0400

    Comentarios eliminados

--- VoiceMail/src/ucb/voicemail/main/ConsoleTelephone.java
+++ VoiceMail/src/ucb/voicemail/main/ConsoleTelephone.java
@@ -1,37 +1,20 @@
 package ucb.voicemail.main;
 import java.util.Scanner;
 
-/**
-   A telephone that takes simulated keystrokes and voice input
-   from the user and simulates spoken text.
-*/
 public class ConsoleTelephone implements Telephone
 {
-   /**
-      Construct phone object.
-      @param aScanner that reads text from a character-input stream
-   */
+
    public ConsoleTelephone(Scanner aScanner)
    {
       scanner = aScanner;
    }
 
-   /**
-      Speak a message to System.out.
-      @param output the text that will be "spoken"
-   */
    @Override
    public void speak(String output)
    {
       System.out.println(output);
    }
 
-   /**
-      Loops reading user input and passes the input to the
-      Connection object's methods dial, record or hangup.
-      @param c the connection that connects this phone to the
-      voice mail system
-   */
    public void run(Connection c)
    {
       boolean more = true;
