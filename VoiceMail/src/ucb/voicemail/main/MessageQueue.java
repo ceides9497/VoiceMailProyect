a278ffcc40b4959ae7539f53f5cdde0dbbfc7e93
SamuelUCB <val97samuel@hotmail.com>
2018-05-07 03:39:28 -0300

    Added comments in MessageQueue

--- VoiceMail/src/ucb/voicemail/main/MessageQueue.java
+++ VoiceMail/src/ucb/voicemail/main/MessageQueue.java
@@ -13,18 +13,22 @@ public class MessageQueue {
 		queue = new ArrayList<Message>();
 	}
 
+	//REMOVE FIRST MESSAGE
 	public Message remove() {
 		return queue.remove(0);
 	}
 
+	// ADD LAST MESSAGE
 	public void add(Message newMessage) {
 		queue.add(newMessage);
 	}
 
+	// GET COUNT OF MESSAGES
 	public int size() {
 		return queue.size();
 	}
 
+	// GET FIRST MESSAGE OR NULL
 	public Message peek() {
 		if (queue.size() == 0) {
 			return null;
