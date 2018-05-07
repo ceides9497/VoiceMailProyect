2f93829f615ada577b491b4cb183ba7292c4d472
SamuelUCB <val97samuel@hotmail.com>
2018-05-07 03:25:15 -0300

    Order in Mailbox

--- VoiceMail/src/ucb/voicemail/main/Mailbox.java
+++ VoiceMail/src/ucb/voicemail/main/Mailbox.java
@@ -5,68 +5,66 @@ import ucb.voicemail.main.MessageQueue;
 
 public class Mailbox {
 
-   public Mailbox(String aPasscode, String aGreeting)
-   {
-      passcode = aPasscode;
-      greeting = aGreeting;
-      newMessages = new MessageQueue();
-      keptMessages = new MessageQueue();
-   }
+    public Mailbox(String aPasscode, String aGreeting) {
+        passcode = aPasscode;
+        greeting = aGreeting;
+        newMessages = new MessageQueue();
+        keptMessages = new MessageQueue();
+    }
 
-   public boolean checkPasscode(String aPasscode)
-   {
-      return aPasscode.equals(passcode);
-   }
+    public boolean checkPasscode(String aPasscode) {
+        return aPasscode.equals(passcode);
+    }
 
-   public void addMessage(Message aMessage)
-   {
-      newMessages.add(aMessage);
-   }
+    public void addMessage(Message aMessage) {
+        newMessages.add(aMessage);
+    }
 
-   public Message getCurrentMessage()
-   {
-      if (newMessages.size() > 0)
-         return newMessages.peek();
-      else if (keptMessages.size() > 0)
-         return keptMessages.peek();
-      else
-         return null;
-   }
+    public Message getCurrentMessage() {
+        if (newMessages.size() > 0) {
+            return newMessages.peek();
+        }
+        else if (keptMessages.size() > 0) {
+            return keptMessages.peek();
+        }
+        else {
+            return null;
+        }
+    }
 
-   public Message removeCurrentMessage()
-   {
-      if (newMessages.size() > 0)
-         return newMessages.remove();
-      else if (keptMessages.size() > 0)
-         return keptMessages.remove();
-      else
-         return null;
-   }
+    public Message removeCurrentMessage() {
+        if (newMessages.size() > 0) {
+            return newMessages.remove();
+        }
+        else if (keptMessages.size() > 0) {
+            return keptMessages.remove();
+        }
+        else {
+            return null;
+        }
+    }
 
-   public void saveCurrentMessage()
-   {
-      Message m = removeCurrentMessage();
-      if (m != null)
-         keptMessages.add(m);
-   }
+    public void saveCurrentMessage() {
+        Message m = removeCurrentMessage();
+        if (m != null) {
+            keptMessages.add(m);
+        }
+    }
 
-   public void setGreeting(String newGreeting)
-   {
-      greeting = newGreeting;
-   }
+    public void setGreeting(String newGreeting) {
+        greeting = newGreeting;
+    }
 
-   public void setPasscode(String newPasscode)
-   {
-      passcode = newPasscode;
-   }
+    public void setPasscode(String newPasscode) {
+        passcode = newPasscode;
+    }
 
-   public String getGreeting()
-   {
-      return greeting;
-   }
+    public String getGreeting() {
+        return greeting;
+    }
 
-   private MessageQueue newMessages;
-   private MessageQueue keptMessages;
-   private String greeting;
-   private String passcode;
+    private MessageQueue newMessages;
+    private MessageQueue keptMessages;
+    private String greeting;
+    private String passcode;
 }
