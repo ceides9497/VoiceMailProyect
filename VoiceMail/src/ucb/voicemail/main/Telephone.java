59c8ee6a2b3d026754ff5c00142f441c883d5fe0
SamuelUCB <val97samuel@hotmail.com>
2018-05-07 02:03:29 -0300

    Added imports in Telephone

--- VoiceMail/src/ucb/voicemail/main/Telephone.java
+++ VoiceMail/src/ucb/voicemail/main/Telephone.java
@@ -1,5 +1,24 @@
 package ucb.voicemail.main;
 
+import ucb.voicemail.main.ChangeGreetingState;
+import ucb.voicemail.main.ChangePasscodeState;
+import ucb.voicemail.main.ConnectedState;
+import ucb.voicemail.main.Connection;
+import ucb.voicemail.main.ConnectionState;
+import ucb.voicemail.main.ConsoleTelephone;
+import ucb.voicemail.main.GraphicalTelephone;
+import ucb.voicemail.main.Mailbox;
+import ucb.voicemail.main.MailboxMenuState;
+import ucb.voicemail.main.MailSystem;
+import ucb.voicemail.main.MailSystemTester;
+import ucb.voicemail.main.MainMenu;
+import ucb.voicemail.main.Message;
+import ucb.voicemail.main.MessageMenuState;
+import ucb.voicemail.main.MessageQueue;
+import ucb.voicemail.main.RecordingState;
+import ucb.voicemail.main.Subject;
+import ucb.voicemail.main.Telephone;
+
 public interface Telephone {
 	void speak(String output);
 }
