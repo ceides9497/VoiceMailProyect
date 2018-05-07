aadb2f2baea2f9050d23ce421207b051a6b9c2e3
SamuelUCB <val97samuel@hotmail.com>
2018-05-07 04:23:50 -0300

    Order MailSystemTester

--- VoiceMail/src/ucb/voicemail/main/MailSystemTester.java
+++ VoiceMail/src/ucb/voicemail/main/MailSystemTester.java
@@ -2,25 +2,20 @@ package ucb.voicemail.main;
 
 import java.util.Scanner;
 
-/**
-   This program tests the mail system. A single phone
-   communicates with the program through System.in/System.out.
-*/
-public class MailSystemTester
-{
-   public static void main(String[] args)
-   {
-	  GraphicalTelephone w = new GraphicalTelephone(new MainMenu());
-      MailSystem system = new MailSystem(MAILBOX_COUNT);
-      Scanner console = new Scanner(System.in);
-      ConsoleTelephone p = new ConsoleTelephone(console);
-      Connection c = new Connection(system, new ConnectedState());
-      c.addUserInterface(p);
-      c.addUserInterface(w);
-      c.start();		// REINICIA LA CONEXION PARA QUE APAREZCA "Enter mailbox number followed by #"
-      w.run(c);
-      p.run(c);
-   }
+public class MailSystemTester {
+    
+    public static void main(String[] args) {
+        GraphicalTelephone w = new GraphicalTelephone(new MainMenu());
+        MailSystem system = new MailSystem(MAILBOX_COUNT);
+        Scanner console = new Scanner(System.in);
+        ConsoleTelephone p = new ConsoleTelephone(console);
+        Connection c = new Connection(system, new ConnectedState());
+        c.addUserInterface(p);
+        c.addUserInterface(w);
+        c.start();		// REINICIA LA CONEXION PARA QUE APAREZCA "Enter mailbox number followed by #"
+        w.run(c);
+        p.run(c);
+    }
 
-   private static final int MAILBOX_COUNT = 20;
+    private static final int MAILBOX_COUNT = 20;
 }
