2a5b82d7f5660238a8bae8085d138231cbf87782
SamuelUCB <val97samuel@hotmail.com>
2018-05-09 08:59:40 -0300

    Added nothing

--- VoiceMail/src/ucb/voicemail/main/Subject.java
+++ VoiceMail/src/ucb/voicemail/main/Subject.java
@@ -1,7 +1,6 @@
 package ucb.voicemail.main;
 
 public interface Subject {
-	
 	public void addUserInterface(Telephone userInterface);
 	public void deleteUserInterface(Telephone userInterface);
 	public void speakToAll(String output);
