459ea984b021affb7eb1eb1fc66c830675484569
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-05-13 21:37:11 -0400

    Clase OptionsList creada

---
+++ VoiceMail/src/ucb/voicemail/menuOptions/OptionsList.java
@@ -0,0 +1,24 @@
+package ucb.voicemail.menuOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionsList {
+	private List<String> list;
+	
+	public OptionsList() {
+		list = new ArrayList<>();
+	}
+	
+	public void add(String option) {
+		list.add(option);
+	}
+	
+	public int size() {
+		return list.size();
+	}
+	
+	public String get(int index) {
+		return list.get(index);
+	}
+}
