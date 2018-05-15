05f3a822239c08eb27f686a8850387c747f2ce2a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-05-15 00:08:30 -0400

    Metodo initialise creado

--- VoiceMail/src/ucb/voicemail/domain/MailSystemTester.java
+++ VoiceMail/src/ucb/voicemail/domain/MailSystemTester.java
@@ -1,7 +1,10 @@
 package ucb.voicemail.domain;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 import ucb.voicemail.domain.connection.state.ConnectedState;
@@ -57,9 +60,46 @@ public class MailSystemTester {
     	initialise();
     }
 
-    private static void initialise() {
-		// TODO Auto-generated method stub
-		
+    private static void initialise() throws SQLException {
+    	Statement state = sqliteConnection.createStatement();
+    	Statement state2 = sqliteConnection.createStatement();
+    	Statement state3 = sqliteConnection.createStatement();
+    	String query = "SELECT name FROM sqlite_master WHERE type='table' AND name='mailbox'";
+    	String query2 = "SELECT name FROM sqlite_master WHERE type='table' AND name='kept_message'";
+    	String query3 = "SELECT name FROM sqlite_master WHERE type='table' AND name='new_message'";
+    	ResultSet res = state.executeQuery(query);
+    	ResultSet res2 = state2.executeQuery(query2);
+    	ResultSet res3 = state3.executeQuery(query3);
+    	
+    	if (!res.next()) {
+    		Statement state4 = sqliteConnection.createStatement();
+			state4.execute("CREATE TABLE mailbox(id integer,"
+					+ "passcode varchar(200)," + "greeting varchar(200),"
+					+ "primary key(id));");
+			
+			for (int i = 0; i < 5; i++) {
+				PreparedStatement prep = sqliteConnection.prepareStatement("INSERT INTO mailbox values(?,?,?);");
+				prep.setString(2, "" + (i + 1));
+				prep.setString(3, "You have reached mailbox " + (i + 1) + ". \r\n" + 
+						"Please leave a message now.");
+				prep.execute();
+			}	
+    	}
+    	
+    	if (!res2.next()) {
+    		Statement state4  = sqliteConnection.createStatement();
+			state4.execute("CREATE TABLE kept_message(id integer,"
+					+ "text varchar(200)," + "mailbox_id integer,"
+					+ "primary key(id));");
+    	}
+    	
+    	if (!res3.next()) {
+    		Statement state4  = sqliteConnection.createStatement();
+			state4.execute("CREATE TABLE new_message(id integer,"
+					+ "text varchar(200)," + "mailbox_id integer,"
+					+ "primary key(id));");
+    	}
+    	
 	}
 
 	private static final int MAILBOX_COUNT = 20;
